d8e893f22393ea47d46f8af1d419063fceea9be3
Bhargav Ganta <bhargavganta2605@gmail.com>
2026-03-22 11:46:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -166,3 +166,42 @@ document.addEventListener("DOMContentLoaded", () => {
     observer.observe(stat);
   }
 });
+document.getElementById("contact-form").addEventListener("submit", async function(e) {
+  e.preventDefault();
+
+  const formData = {
+    name: this.name.value,
+    email: this.email.value,
+    subject: this.subject.value,
+    message: this.message.value
+  };
+
+  try {
+    const res = await fetch("http://localhost:5000/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    });
+
+    const text = await res.text();
+    alert(text);
+    this.reset(); // clear form
+  } catch (err) {
+    alert("Error sending message");
+  }
+});
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  emailjs.sendForm("service_2e4uxsg", "template_lfv5ssr", this)
+    .then(function() {
+      alert("✅ Message sent successfully!");
+    }, function(error) {
+      alert("❌ Failed to send message");
+      console.log(error);
+    });
+
+  this.reset();
+});
